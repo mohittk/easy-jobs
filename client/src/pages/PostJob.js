bc07e609549033197c02a57af0609a0a885a54da
mohittk <mvk1407@gmail.com>
2022-04-09 19:59:56 +0530

    update

--- client/src/pages/PostJob.js
+++ client/src/pages/PostJob.js
@@ -20,37 +20,58 @@ export default function PostJob() {
         </h1>
       </div>
 
-  
-
       <div className="post-job-container text-left m-20  p-20 rounded-xl  bg-[#ffffff]">
-      <div className="title ">
-          <h1 className="m-20 text-3xl font-semibold"> Job posting Form </h1>
-      </div>
-          <div className="role">
-        <label className="text-xl ml-20 font-medium ">Enter the role </label>
-        <input type="text " className="role shadow-2xl p-3 w-[20%] text-xl m-3 bg-white border-2 rounded-xl" />
+        <div className="title ">
+          <h1 className="m-20 text-3xl font-semibold text-indigo-600">
+            {" "}
+            Job posting Form{" "}
+          </h1>
+        </div>
+        <div className="role">
+          <label className="text-xl ml-20 font-medium ">Enter the role </label>
+          <input
+            type="text"
+            onChange={((e)=>{setRole(e.target.value);})}
+            className="role shadow-2xl p-3 w-[20%] text-xl m-3 bg-white  outline-none border-2 rounded-xl"
+          />
         </div>
 
         <div className="jobtype">
-            <label className="jobtype ml-20 text-xl font-medium ">Select job type </label>
-        <select className="job-type shadow-2xl p-3 w-[20%] text-xl border-2 m-3 bg-white outline-none rounded-xl">
-          <option value="1">Full-Time</option>
-          <option value="1">Part-Time</option>
-        </select>
+          <label className="jobtype ml-20 text-xl font-medium ">
+            Select job type{" "}
+          </label>
+          <select onChange={((e)=>{setJobType(e.target.value)})}   className="job-type shadow-2xl p-3 w-[20%] text-xl border-2 m-3 bg-white outline-none rounded-xl">
+            <option value="1">Full-Time</option>
+            <option value="1">Part-Time</option>
+          </select>
         </div>
 
         <div className="company">
-            <label  className="text-xl ml-20 font-medium">Enter Your company name</label>
-            <input type="text" className="company shadow-2xl p-3 w-[20%] text-xl border-2 m-3 bg-white outline-none rounded-xl" />
-        </div>
-        
-        <div className="location">
-        <label className="text-xl ml-20 font-medium" > Enter Location </label>
-        <input type="text" className="location p-3 border-2 shadow-2xl w-[20%] text-xl m-5 bg-white outline-none rounded-xl" />
+          <label className="text-xl ml-20 font-medium">
+            Enter Your company name
+          </label>
+          <input
+            type="text"
+            onChange={((e)=>{setCompany(e.target.value)})}
+            className="company shadow-2xl p-3 w-[20%] text-xl border-2 m-3 bg-white outline-none rounded-xl"
+          />
         </div>
 
-        <button className="submit p-3 border-2 shadow-2xl w-[20%] text-xl hover:bg-[#c0c0c0] ml-20 bg-white outline-none rounded-xl " type="submit" >
-            Submit
+        <div className="location">
+          <label className="text-xl ml-20 font-medium"> Enter Location </label>
+          <input
+            type="text"
+            onChange={((e)=>{setLocation(e.target.value)})}
+            className="location p-3 border-2 shadow-2xl w-[20%] text-xl m-5 bg-white outline-none rounded-xl"
+          />
+        </div>
+
+        <button
+          className="submit p-3 border-2 shadow-2xl w-[20%] text-xl hover:bg-[#c0c0c0] ml-20 bg-white outline-none rounded-xl "
+          type="submit"
+          onClick={((e)=>{setLocation(''); setCompany(''); setJobType(''); setRole('');})}
+        >
+          Submit
         </button>
       </div>
     </>
