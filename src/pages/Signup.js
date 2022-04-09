ba7e687479fecfaba5d75b41e8319c05a6ad7d59
unknown <ujjgpta9@gmail.com>
2022-04-09 14:11:41 +0530

    issue form done , title for each page get displayed

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 
 export default function Signup() {
+  document.title="Signup | IIIT-Bhopal Library";
   return (
     <>
       <h1 className="text-blue-800 rounded-xl shadow-2xl text-3xl p-10 w-[25rem] font-alfa ml-10">
