08935f71b886bacf3dde87c5514cce49badcecb0
mohittk <mvk1407@gmail.com>
2022-04-09 17:40:55 +0530

    update

---
+++ client/src/components/JobContainer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+export default function JobContainer(props){
+    return(
+        <>
+        <div className="job-container flex flex-row m-10 shadow-2xl">
+            <div className="role w-[50%] p-3 text-2xl font-medium ">{props.role}
+            <div className="company text-lg p-1 bg-indigo-600 text-white rounded w-[35%] text-center">
+                {props.company}</div>
+                </div>
+            <div className="job-type m-3 w-[50%] text-xl font-medium p-3">
+                {props.location}
+            </div>
+            <button className="check m-3 w-[50%] bg-indigo-600 rounded-xl text-white font-medium p-3">
+                Check
+            </button>
+        </div>
+        </>
+
+
+    )
+
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
 
-
-export default function Home(){
-    return(
-        <>
-        <div className="home-page ">
-            <div className="upperbar bg-indigo-600">
-                <div className="nav float-right p-[3.5rem] text-3xl font-encode text-white">
-                <Navbar />
-                </div>
-                <h1 className="text-6xl text-white  shadow-2xl font-medium p-10 font-titan"> Easy Jobs
-            </h1>
-
-           
-           </div>
+export default function Home() {
+  return (
+    <>
+      <div className="home-page ">
+        <div className="upperbar bg-indigo-600">
+          <div className="nav float-right p-[3.5rem] text-3xl font-encode text-white">
+            <Navbar />
+          </div>
+          <h1 className="text-6xl text-white  shadow-2xl font-medium p-10 font-titan">
+            {" "}
+            Easy Jobs
+          </h1>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
