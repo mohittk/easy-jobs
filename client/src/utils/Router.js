4909019f2ff2e1be4fda8d558b527f19eb038318
unknown <ujjgpta9@gmail.com>
2022-04-10 15:04:23 +0530

    new changes

--- client/src/utils/Router.js
+++ client/src/utils/Router.js
@@ -8,7 +8,7 @@ import PostJob from '../pages/PostJob';
 import Signup from '../pages/Signup';
 import Login from '../pages/Login';
 import Dashboard from '../pages/Dashboard';
-import Profile from '../pages/Profile';
+
 
 
 
@@ -22,9 +22,9 @@ export default function Router() {
           <Route exact path="/postjob" element={<PostJob />} />
           <Route exact path="/signup" element={<Signup />} />
           <Route exact path="/login" element={<Login />} />
-          <Route exact path="/dashboard" element={<Dashboard />} />
-          <Route exact path="/profile" element={<Profile />} />
-      
+          <Route exact path="/applicant/dashboard" element={(localStorage.getItem("applicant_token") ? <Dashboard /> : <Home />)} />
+          <Route exact path="/recruiter/dashboard" element={(localStorage.getItem("recruiter_token") ? <Dashboard /> : <Home />)} />
+
         </Routes>
       </BrowserRouter>
     </>
